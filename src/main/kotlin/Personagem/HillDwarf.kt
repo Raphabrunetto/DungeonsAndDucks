8795cfb6bc50cbb8324083906ba7cc0d80201e19
Raphael <raphabrunetto@gmail.com>
2024-10-14 22:50:37 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main/kotlin/Personagem/HillDwarf.kt
+++
@@ -1,9 +0,0 @@
-package personagem
-
-import Interfaces.IRaca
-
-class HillDwarf(override val nome: String) : IRaca {
-    override val name = "Hill Dwarf"
-    override val description = "Sabedoria + 1"
-    override val bonusStats = mapOf("Sabedoria" to 1)
-}
